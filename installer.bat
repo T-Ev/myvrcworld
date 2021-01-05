1baccdbc27ec696e6d4eaf8383daa731e1e73baf
TJEv <tng22@wildcats.unh.edu>
2021-01-05 13:58:34 -0500

    touch up

--- installer.bat
+++ installer.bat
@@ -1,5 +1,7 @@
 @echo off
 echo installer started
+echo installing chocolaty...
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 python --version
 if ERRORLEVEL 1 GOTO NOPYTHON  
 goto :HASPYTHON  
@@ -10,11 +12,39 @@ curl -LJO "https://raw.githubusercontent.com/T-Ev/myvrcworld/main/installer-powe
 ::Powershell.exe -File X:installer-powershell.ps1 -version 3.9.1
 echo installing python...
 powershell -File installer-powershell2.ps1
-echo Wait for Python install to finish in seperate window then proceed...
-pause
+echo python installed successfully.
+refreshenv
 
 :HASPYTHON 
 echo haspython... continuing
-curl -LJO "https://raw.githubusercontent.com/T-Ev/myvrcworld/main/finishinstall.bat"
-start cmd.exe \k "finishinstall.bat"
+echo creating program folder
+mkdir myvrcworld
+cd myvrcworld
+mkdir static
+cd static
+curl -LJO "https://raw.githubusercontent.com/T-Ev/myvrcworld/main/static/myvrcworld.html"
+cd ..
+echo installing venv...
+python -m venv venv
+echo downloading and installing activation script
+cd venv\Scripts
+del activate.bat
+curl -LJO "https://raw.githubusercontent.com/T-Ev/myvrcworld/main/activate.bat"
+curl -LJO "https://raw.githubusercontent.com/T-Ev/myvrcworld/main/activate-install.bat"
+cd ..
+cd ..
+::cd ..
+::xcopy /Y myvrcworld.py myvrcworld\
+::cd myvrcworld
+curl -LJO "https://raw.githubusercontent.com/T-Ev/myvrcworld/main/myvrcworld.py"
+echo downloading launcher
+curl -LJO "https://raw.githubusercontent.com/T-Ev/myvrcworld/main/launcher.bat"
+echo launching local server
+start cmd.exe /k "venv\Scripts\activate-install.bat"
+echo Ready to launch myvrcworld?
+TIMEOUT /T 6
+echo launching obs display
+python -m webbrowser http://localhost:5000/myvrcworld.html
+echo installation successful!
+echo to launch this app in the future use launcher.bat in the folder myvrcworld
 cmd /k
