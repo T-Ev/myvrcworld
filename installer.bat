ccb1b18e46bbd67b2f9192769e9a7ade0784f8b3
TJEv <tng22@wildcats.unh.edu>
2021-01-05 16:49:55 -0500

    updating activation

--- installer.bat
+++ installer.bat
@@ -1,22 +1,23 @@
 @echo off
 echo installer started
+echo creating program folder
+mkdir myvrcworld
+cd myvrcworld
 echo installing chocolaty...
 ::@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-python --version
+Python\python.exe --version
 if ERRORLEVEL 1 GOTO NOPYTHON  
 goto :HASPYTHON  
 :NOPYTHON
 echo nopython
 echo downloading python via powershell
-curl -LJO "https://raw.githubusercontent.com/T-Ev/myvrcworld/main/installer-powershell2.ps1"
-::Powershell.exe -File X:installer-powershell.ps1 -version 3.9.1
 echo installing python...
-powershell -File installer-powershell2.ps1
-::PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%cd%\installer-powershell2.ps1""' -Verb RunAs}"
+curl -LJO "https://github.com/T-Ev/myvrcworld/raw/main/Python.zip"
+mkdir Python
+powershell.exe -nologo -noprofile -command "& { Expand-Archive -Path Python.zip -DestinationPath %cd%\Python }"
 echo python installer closed
-call refreshenv
 VERIFY > nul
-python --version
+Python\python.exe --version
 if ERRORLEVEL 1 GOTO PYTHONFAIL
 goto :PYTHONSUCCESS
 :PYTHONFAIL
@@ -29,15 +30,12 @@ echo python installed successfully
 
 :HASPYTHON
 echo haspython... continuing
-echo creating program folder
-mkdir myvrcworld
-cd myvrcworld
 mkdir static
 cd static
 curl -LJO "https://raw.githubusercontent.com/T-Ev/myvrcworld/main/static/myvrcworld.html"
 cd ..
 echo installing venv...
-python -m venv venv
+Python\python.exe -m venv venv
 echo downloading and installing activation script
 cd venv\Scripts
 del activate.bat
@@ -56,7 +54,7 @@ start cmd.exe /k "venv\Scripts\activate-install.bat"
 echo Ready to launch myvrcworld?
 TIMEOUT /T 10
 echo launching obs display
-python -m webbrowser http://localhost:5000/myvrcworld.html
+Python\python.exe -m webbrowser http://localhost:5000/myvrcworld.html
 echo installation successful!
 echo to launch this app in the future use launcher.bat in the folder myvrcworld
 cmd /k
